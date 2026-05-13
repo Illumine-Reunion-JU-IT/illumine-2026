1f7435524af8f32dc56af3719d457fa436268edf
Krishagarwal200 <krish5932n@gmail.com>
2026-05-14 01:08:12 +0530

    NewsLetter Drive link Updated

--- app/sections/Hero.tsx
+++ app/sections/Hero.tsx
@@ -52,7 +52,7 @@ const Hero: React.FC = () => {
 >
     <div className="absolute inset-0 border-4 border-white pointer-events-none" />
     <a 
-        href="https://drive.google.com/file/d/12Mm7dJbenzOH1eOHfEo-tZb07NLZ1k9N/view?usp=sharing" 
+        href="https://drive.google.com/file/d/1EWQH2PHl4hqjfktKXWtzI6ykDdpQu07s/view?usp=drive_link" 
         target="_blank"
         rel="noopener noreferrer"
         className="flex items-center justify-center font-tt-lakes font-medium w-full h-full bg-transparent text-white text-xs sm:text-sm hover:bg-white/10 transition-colors cursor-pointer"
